1664206a9affb6869b126f450f9d1b5af95bb26d
yusufyaldiz3416 <166456583+yusufyaldiz3416@users.noreply.github.com>
2026-03-12 18:09:25 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:09:22.959Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-12T15:09:25.414Z
+Commit: Resolve timezone issues in date handling
