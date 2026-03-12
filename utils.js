b7d0fb53e48fac7c04c6541e4025ca89dfd57cea
yusufyaldiz3416 <166456583+yusufyaldiz3416@users.noreply.github.com>
2026-03-12 18:08:39 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:08:25.886Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-12T15:08:39.166Z
+Commit: Fix broken links in documentation
