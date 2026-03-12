18502122e86dc8696a09f7b6beb082aa56960d0f
yusufyaldiz3416 <166456583+yusufyaldiz3416@users.noreply.github.com>
2026-03-12 18:02:38 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:02:36.920Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-12T15:02:38.352Z
+Commit: Patch API endpoint error handling
