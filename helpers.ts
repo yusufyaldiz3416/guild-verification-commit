c9c2f6acac902cc45762db55e968d03bf0725006
yusufyaldiz3416 <166456583+yusufyaldiz3416@users.noreply.github.com>
2026-03-12 18:03:16 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:02:49.126Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-12T15:03:15.990Z
+Commit: Patch API endpoint error handling
