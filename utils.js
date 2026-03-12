39ebfc58dc4bdee659c5c51b45683363c7e61a50
yusufyaldiz3416 <166456583+yusufyaldiz3416@users.noreply.github.com>
2026-03-12 18:08:57 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:08:51.716Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-12T15:08:57.109Z
+Commit: Fix broken links in documentation
