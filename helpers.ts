12c31252fe4e213216f112a572ad6b2a4d2ce483
yusufyaldiz3416 <166456583+yusufyaldiz3416@users.noreply.github.com>
2026-03-12 18:08:27 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:03:23.660Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-12T15:08:27.179Z
+Commit: Resolve null pointer exception in user service
