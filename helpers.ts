7015d959709dd9513775faa85063b7497ba68f36
yusufyaldiz3416 <166456583+yusufyaldiz3416@users.noreply.github.com>
2026-03-12 18:09:26 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:09:16.076Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-12T15:09:26.573Z
+Commit: Implement user profile customization
