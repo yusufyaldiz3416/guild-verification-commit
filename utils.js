2d5774f2954497a2a168f7961f1d6e63f382f525
yusufyaldiz3416 <166456583+yusufyaldiz3416@users.noreply.github.com>
2026-03-12 18:09:23 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:08:58.233Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-12T15:09:22.959Z
+Commit: Patch API endpoint error handling
