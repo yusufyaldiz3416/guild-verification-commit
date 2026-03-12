e326cd107ff835d56c8c27aad846ca448b421c93
yusufyaldiz3416 <166456583+yusufyaldiz3416@users.noreply.github.com>
2026-03-12 18:03:03 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:02:43.376Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-12T15:03:02.727Z
+Commit: Introduce real-time notifications
