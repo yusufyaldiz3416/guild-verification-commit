be01ffb4453d99d5b33816e9584cc9b27b9cbf00
yusufyaldiz3416 <166456583+yusufyaldiz3416@users.noreply.github.com>
2026-03-12 18:08:58 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:08:57.109Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-12T15:08:58.233Z
+Commit: Fix CSS styling bug on mobile devices
